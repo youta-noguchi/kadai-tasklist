a6397475f82f1c288c72d526f1d7617d64191ee1
yota.noguchi <y.nogu2045@icloud.com>
2020-07-14 03:55:12 +0000

    kadai add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,10 +45,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -95,10 +97,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $request->validate([
-            'content'=> 'required|max:255',
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
         ]);
         
         $task = Task::findOrFail($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
