94d557707cf35e83f9569a5bb7d8337b8983f80f
yota.noguchi <y.nogu2045@icloud.com>
2020-07-14 03:26:46 +0000

    kadai validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -90,6 +94,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'content'=> 'required|max:255',
+        ]);
+        
         $task = Task::findOrFail($id);
         $task->content = $request->content;
         $task->save();
